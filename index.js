4a385d6e2e4758892ee503251e8c53964a8ba6be
wael <waeelash155@gmail.com>
2022-03-08 17:17:09 -0500

    first draft

---
+++ index.js
@@ -0,0 +1,7 @@
+const likeButton = document.querySelector('#Like-Btn')
+
+likeButton.addEventListener('click', (e) => {
+    console.log('hi')
+})
+
+const movieImg = document.querySelector('#poster')
