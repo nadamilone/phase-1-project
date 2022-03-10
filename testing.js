014c4997a112398f5271fdab48c7d9dc36f862cb
NADA MILONE <nadamilone@gmail.com>
2022-03-10 08:40:22 -0500

    first css commit

--- testing.js
+++ testing.js
@@ -3,3 +3,58 @@
 // fetch("https://ghibliapi.herokuapp.com")
 //   .then((response) => response.json())
 //   .then((data) => console.log(data));
+
+// #main-div {
+//   border: 4px solid purple;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   flex-direction: column;
+// }
+
+// h1 {
+//   font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande";
+//   color: tomato;
+//   text-align: center;
+// }
+
+// h2 {
+//   font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande";
+//   color: greenyellow;
+//   text-align: center;
+// }
+
+// h3 {
+//   font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande";
+//   color: blueviolet;
+//   text-align: center;
+// }
+
+// #movie-details {
+//   font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande";
+//   align-self: flex-end;
+//   width: 75%;
+//   height: 60vh;
+//   border: 2px solid red;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-direction: column;
+//   background-color: whitesmoke;
+//   opacity: 0.8;
+//   margin-top: 10px;
+//   box-shadow: 5px 10px;
+// }
+// .fun-time {
+//   border: 1px solid black;
+//   padding: 50px;
+//   width: 200px;
+//   height: 20vh;
+//   background-color: seagreen;
+//   margin-top: 10px;
+//   margin-bottom: 5px;
+// }
+
+// #movie-list {
+//   border: 1px solid red;
+// }
